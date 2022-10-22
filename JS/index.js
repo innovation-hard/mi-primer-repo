0a6d2f5dfd49bde91c010337ce07c4a17d8742cd
Darío Wietrznik <Dario.wietr@gmai.com>
2022-10-22 18:05:18 -0300

    integracion animaciones y js

---
+++ JS/index.js
@@ -0,0 +1,26 @@
+let cad=`
+<h1 id="titulo">Entrenamiento con Kettlebells</h1>
+            <nav>
+                <br>
+                <a class="nav" href="index.html">Home</a>
+                <a class="nav" href="about.html">Acerca de</a>
+                <a class="nav" href="form.html">Regístrese</a>
+                <a class="nav" href="location.html">Sucursales</a>
+            
+            </nav>
+
+`
+document.getElementById("idheader").innerHTML=cad;
+
+
+cad=`
+<a class="contacto" href="mailto:jpserra@live.com.ar">Contacto</a>
+            <a class="arriba" href="#titulo">Arriba</a>
+            <a class="redsoc" href="https://www.twitter.com" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+            <a class="redsoc" href="https://www.facebook.com" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+            <a class="redsoc" href="https://www.pinterest.com" target="_blank"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
+            <br>
+            <p class="derechos">Todos los derechos reservados.</p>
+
+        `
+        document.getElementById("idfooter").innerHTML=cad;
